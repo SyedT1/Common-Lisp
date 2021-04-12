f90a6b70edc5620b93cae486d0ad2eb9dc8c2d16
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2021-04-12 13:58:53 +0600

    input based operations{multiply,addition,division}

---
+++ Before Basics/003.lisp
@@ -0,0 +1,8 @@
+(defun addnums ()
+  (princ "Add two numbers = ")
+  (write (+ (read) (read))))
+(defun multiply ()
+  (princ "Multiply two numbers = ")(write (* (read)(read))))
+(defun divide ()
+  (princ "Divide two numbers = ")
+  (write (/ (read) (read))))
